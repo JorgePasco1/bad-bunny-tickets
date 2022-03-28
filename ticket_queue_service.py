28602f95e9825e209832570f294fdb603aa8e48c
jorgepasco1 <jorgepascosoto@gmail.com>
2022-03-28 08:08:50 -0500

    go directly to queue url

--- ticket_queue_service.py
+++ ticket_queue_service.py
@@ -14,9 +14,11 @@ def multiple_queueing(number_of_queues: int):
         """Starts a new driver and enters the queue."""
 
         driver = start_webdriver()
-        driver.get(TICKETS_URL)
-        buy_button = driver.find_element(By.XPATH, BUY_BUTTON_XPATH)
-        buy_button.click()
+        # driver.get(TICKETS_URL)
+        # buy_button = driver.find_element(By.XPATH, BUY_BUTTON_XPATH)
+        # buy_button.click()
+        driver.get(
+            'https://queue.teleticket.com.pe/?c=puntoticket&e=ics014&t_cal=1&t_ct=2')
 
     for i in range(number_of_queues + 1):
         print(f"Opening browser {i + 1}")
